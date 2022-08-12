706ab6d89ce57ce139b47a26a35428148368fdfc
IvanovRoman <meninyellow8@gmail.com>
2022-08-12 11:04:24 +0300

    Задача4

---
+++ Задача4/Program.cs
@@ -0,0 +1,16 @@
+﻿double number1 = Convert.ToDouble(Console.ReadLine());
+double number2 = Convert.ToDouble(Console.ReadLine());
+double number3 = Convert.ToDouble(Console.ReadLine());
+double max;
+if (number1 >= number2){
+    max = number1;
+}
+else {
+    max = number2;
+}
+if (number3 >= max){
+    Console.WriteLine($"max = {number3}");
+}
+else {
+    Console.WriteLine($"max = {max}");
+}
