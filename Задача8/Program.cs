1cbc6cb9372385a2fbfac31b99fcc247548f365b
IvanovRoman-T <110655639+IvanovRoman-T@users.noreply.github.com>
2022-08-16 09:45:54 +0300

    Delete Задача8 directory

--- Задача8/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
-for (int i = 2; i <= number; i += 2){
-    if (i < number-1){
-    Console.Write($"{i}, ");
-    }
-    else {
-    Console.Write(i);
-    }
-}
