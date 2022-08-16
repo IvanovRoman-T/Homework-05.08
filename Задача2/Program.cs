5bcb822f732fabdc2f40e99324a8398b2645f40d
IvanovRoman-T <110655639+IvanovRoman-T@users.noreply.github.com>
2022-08-16 09:45:01 +0300

    Delete Задача2 directory

--- Задача2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿double number1 = Convert.ToDouble(Console.ReadLine());
-double number2 = Convert.ToDouble(Console.ReadLine());
-if (number1 >= number2){
-    Console.WriteLine($"max = {number1}");
-}
-else {
-    Console.WriteLine($"max = {number2}");
-}
