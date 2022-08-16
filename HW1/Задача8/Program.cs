cbaab38929ea1ee02392654bc8b8ecd0e68fd7fc
IvanovRoman <meninyellow8@gmail.com>
2022-08-16 09:43:21 +0300

    new_folders

---
+++ HW1/Задача8/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i += 2){
+    if (i < number-1){
+    Console.Write($"{i}, ");
+    }
+    else {
+    Console.Write(i);
+    }
+}
