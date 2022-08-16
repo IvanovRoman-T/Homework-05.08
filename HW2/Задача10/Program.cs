7cd43b0998be5991cccfa41ad0d351fb3b6f833b
IvanovRoman <meninyellow8@gmail.com>
2022-08-16 10:05:30 +0300

    Задача10

---
+++ HW2/Задача10/Program.cs
@@ -0,0 +1,22 @@
+﻿int ReadNumber(){
+    Console.Write("Введите трёхзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if ((number > 999) || (number < 100))
+    {
+        Console.WriteLine("Число не трёхзначное");
+        return ReadNumber();
+    }
+    else
+    {
+        return number;
+    }
+}
+
+
+void ShowSecondDigit(int number){
+    int secondDigit = (number%100) / 10;
+    Console.WriteLine($"Вторая цифра числа {number} - {secondDigit}");
+}
+
+int number = ReadNumber();
+ShowSecondDigit(number);
