9a71caa6ab7e9798d6241e6401e64eb0675ff1ed
IvanovRoman <meninyellow8@gmail.com>
2022-08-16 10:30:39 +0300

    Задача15

---
+++ HW2/Задача15/Program.cs
@@ -0,0 +1,23 @@
+﻿int ReadWeekDayNumber(){
+    Console.Write("Введите номер дня недели: ");
+    int dayNumber = Convert.ToInt32(Console.ReadLine());
+    if ((dayNumber < 1) || (dayNumber > 7)){
+        Console.WriteLine("Дня недели с таким номером не существует");
+        return ReadWeekDayNumber();
+    }
+    else {
+        return dayNumber;
+    }
+}
+
+void IsItWeekend(int dayNumber){
+    if ((dayNumber == 6) || (dayNumber == 7)){
+        Console.WriteLine("Ура!!! Это выходной!!!");
+    }
+    else{
+        Console.WriteLine("Это не выходной :(");
+    }
+}
+
+int dayNumber = ReadWeekDayNumber();
+IsItWeekend(dayNumber);
