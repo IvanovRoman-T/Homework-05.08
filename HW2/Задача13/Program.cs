e21c3c71087a4fbe52f23ed84e3106c1d1a50b58
IvanovRoman <meninyellow8@gmail.com>
2022-08-16 10:17:18 +0300

    Задача13

---
+++ HW2/Задача13/Program.cs
@@ -0,0 +1,13 @@
+﻿void ShowThirdDidit(string number){
+    if (number.Length < 3){
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else{
+        Console.WriteLine($"Третья цифра числа {number} - {number[2]}");
+    }
+}
+
+
+Console.Write("Введите целое число: ");
+string number = Console.ReadLine();
+ShowThirdDidit(number);
