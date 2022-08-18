84a409642f8ab2c064d588b47c20c6fdec07566f
IvanovRoman <meninyellow8@gmail.com>
2022-08-18 22:00:25 +0300

    Задача21

---
+++ HW3/Задача23/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadNumber(){
+    Console.Write("Введите натуральное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 1){
+        Console.WriteLine("Это не натуральное число");
+        return ReadNumber();
+    }
+    else {
+    return number;
+    }
+}
+
+int[] FindCubes(int number){
+    int[] array = new int[number];
+    for (int i = 1; i <= number; i++){
+        array [i-1] = Convert.ToInt32(Math.Pow(i, 3));
+    }
+    return array;
+}
+
+void PrintArray(int[] array){
+    string str = String.Empty;
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int number = ReadNumber();
+PrintArray(FindCubes(number));
