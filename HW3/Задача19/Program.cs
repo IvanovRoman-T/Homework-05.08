bae61c83985e4068e3015de42e26b9a0176eab37
IvanovRoman <meninyellow8@gmail.com>
2022-08-18 21:09:26 +0300

    Задача19

---
+++ HW3/Задача19/Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadNumber(){
+    Console.Write("Введите пятизначное число: ");
+    string number = Console.ReadLine();
+    if (number.Length != 5){
+        Console.WriteLine("Это не пятизначное число");
+        return ReadNumber();
+    }
+    else {
+    return Convert.ToInt32(number);
+    }
+}
+
+bool IsItPalindrome(string str){
+    int len = str.Length;
+    for (int i = 0; i <= len / 2; i++){
+        if (str[i] != str[len - 1 - i]){
+            return false;
+        }        
+    }
+    return true;
+}
+
+int number = ReadNumber();
+if (IsItPalindrome(Convert.ToString(number))){
+    Console.WriteLine("Это палиндром.");
+}
+else{
+    Console.WriteLine("Это не палиндром.");
+}
