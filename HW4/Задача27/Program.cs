72c1863f01225e5459136162d8cd669da48d87f4
IvanovRoman <meninyellow8@gmail.com>
2022-08-23 14:13:02 +0300

    Задача27

---
+++ HW4/Задача27/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetNumber()
+{
+    Console.Write("Введите целое число: ");
+    if (!(int.TryParse(Console.ReadLine(), out int number)))
+    {
+        return GetNumber();
+    }
+    else
+    {
+        return number;
+    }
+}
+
+int GetSumOfDigits(int number)
+{
+    int o = 0;
+    if (number < 0)
+    {
+        o = 1;
+    }
+    string num = number.ToString();
+    int sum = 0;
+    for (int i = o; i < num.Length; i++)
+    {
+        sum += Convert.ToInt32(Convert.ToString(num[i]));
+    }
+    return sum;
+}
+
+int result = GetSumOfDigits(GetNumber());
+Console.WriteLine(result);
+
+
