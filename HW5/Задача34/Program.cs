915913dc2014b03ee60d93731f0a069255be94d7
IvanovRoman <meninyellow8@gmail.com>
2022-08-26 12:27:14 +0300

    Задача34

---
+++ HW5/Задача34/Program.cs
@@ -0,0 +1,40 @@
+﻿Random random = new Random();
+int[] numbers = new int[random.Next(1, 11)];
+
+
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(100, 1000);
+    }
+    return arr;
+}
+
+
+int CountEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = FillArray(numbers);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine(CountEvenNumbers(array));
