014074a0bec97eabf6b00451a432465f2437dba3
IvanovRoman <meninyellow8@gmail.com>
2022-09-09 09:44:33 +0300

    Задача64

---
+++ HW9/Задача64/Program.cs
@@ -0,0 +1,49 @@
+﻿int GetNumberM()
+{
+    Console.Write("Введите натуральное число M: ");
+    if (!(int.TryParse(Console.ReadLine(), out int number)) || (number < 0))
+    {
+        return GetNumberM();
+    }
+    else
+    {
+        return number;
+    }
+}
+
+
+int GetNumberN(int M)
+{
+    Console.Write("Введите натуральное число N, N > M: ");
+    if (!(int.TryParse(Console.ReadLine(), out int number)) || (number < 0) || (number < M))
+    {
+        return GetNumberN(M);
+    }
+    else
+    {
+        return number;
+    }
+}
+
+
+void ShowNumbersFromMToN(int M, int N, int k = 1)
+{
+    if (k < M)
+    {
+        k = M;
+    }
+    if (k < N)
+    {
+        System.Console.Write($"{k}, ");
+        ShowNumbersFromMToN(M, N, k + 1);
+    }
+    else
+    {
+        System.Console.Write(k);
+    }
+}
+
+
+int m = GetNumberM();
+int n = GetNumberN(m);
+ShowNumbersFromMToN(m, n);
