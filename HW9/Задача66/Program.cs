d32b962c151a7cd699c2b224fd459652dc8fa63f
IvanovRoman <meninyellow8@gmail.com>
2022-09-09 09:50:54 +0300

    Задача66

---
+++ HW9/Задача66/Program.cs
@@ -0,0 +1,49 @@
+﻿int GetNumberM()
+{
+    Console.Write("Введите натуральное число M: ");
+    if (!(int.TryParse(Console.ReadLine(), out int number)) || (number < 0))
+    {
+        return GetNumberM();
+    }
+    else
+    {
+        return number;
+    }
+}
+
+
+int GetNumberN(int M)
+{
+    Console.Write("Введите натуральное число N, N > M: ");
+    if (!(int.TryParse(Console.ReadLine(), out int number)) || (number < 0) || (number < M))
+    {
+        return GetNumberN(M);
+    }
+    else
+    {
+        return number;
+    }
+}
+
+
+int SumNumbersFromMToN(int M, int N, int k = 1, int sum = 0)
+{
+    if (k < M)
+    {
+        k = M;
+    }
+    if (k <= N)
+    {
+        sum += k;
+        return SumNumbersFromMToN(M, N, k + 1, sum);
+    }
+    else
+    {
+        return sum;
+    }
+}
+
+
+int m = GetNumberM();
+int n = GetNumberN(m);
+System.Console.WriteLine(SumNumbersFromMToN(m, n));
